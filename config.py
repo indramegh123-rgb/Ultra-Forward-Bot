809f4bc81466ac3f7f22fedd2e6ab4911456a3e2
indramegh123-rgb <indramegh123@gmail.com>
2026-07-10 22:08:37 +0530

    Update configuration values and remove credits

--- config.py
+++ config.py
@@ -1,20 +1,12 @@
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
-
-
-
 import os
 
 class Config:
     API_ID = os.environ.get("API_ID", "")
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
+    BOT_SESSION = os.environ.get("BOT_SESSION", "Indra-forward-bot") 
     DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+    DB_NAME = os.environ.get("DB_NAME", "IND_BOTZ_FORWARD")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
 
 
@@ -25,14 +17,3 @@ class temp(object):
     BANNED_USERS = []
     IS_FRWD_CHAT = []
     
-
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
