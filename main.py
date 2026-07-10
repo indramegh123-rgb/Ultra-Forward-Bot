df2b4255f3ca9d0826a462e08bb146469694b62e
indramegh123-rgb <indramegh123@gmail.com>
2026-07-10 23:31:03 +0530

    Remove credit comments from main.py
    Removed credit comments from the main.py file.

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
-
-
-
 from bot import Bot
 
 app = Bot()
 app.run()
-
-
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
